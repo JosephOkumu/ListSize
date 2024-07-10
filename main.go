b3f0940bdd82fec80aaf5218a226bd0999e9fff1
JosephOkumu <jokumu25@gmail.com>
2024-07-10 13:15:29 +0300

    Adding comments

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import "fmt"
 
+// NodeL represents a node in the linked list
 type NodeL struct {
-	Data interface{}
-	Next *NodeL
+	Data interface{} // Data stores the value of the node
+	Next *NodeL      // Next points to the next node in the list
 }
 
+// List represents the linked list
 type List struct {
-	Head *NodeL
-	Tail *NodeL
+	Head *NodeL // Head points to the first node in the list
+	Tail *NodeL // Tail points to the last node in the list
 }
 
+// listSize returns the size (number of nodes) of the linked list
 func listSize(l *List) int {
 	count := 0
 	current := l.Head
@@ -23,6 +26,7 @@ func listSize(l *List) int {
 	return count
 }
 
+// listPushFront inserts a new node with the given data at the beginning of the linked list
 func listPushFront(l *List, data interface{}) {
 	newNode := &NodeL{
 		Data: data,
@@ -35,12 +39,15 @@ func listPushFront(l *List, data interface{}) {
 }
 
 func main() {
+	// Create a new linked list
 	link := &List{}
 
+	// Insert nodes at the beginning of the list
 	listPushFront(link, "Hello")
 	listPushFront(link, "2")
 	listPushFront(link, "you")
 	listPushFront(link, "man")
 
+	// Print the size of the linked list
 	fmt.Println(listSize(link))
 }
